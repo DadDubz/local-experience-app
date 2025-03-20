a07ec69dde6b65b1456c5d4a5eae4312f0d270bc
DadDubz <daddubzonfb@gmail.com>
2025-03-20 05:57:14 -0500

    34

---
+++ instrument.js
@@ -0,0 +1,6 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "https://f02939de444cdc81dbc481f70dfe448c@o4509009671815168.ingest.us.sentry.io/4509009675419648",
+});
