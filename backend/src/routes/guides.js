f131299edd6fb2831e8914bbd9b035b7a68cfb94
DadDubz <daddubzonfb@gmail.com>
2025-03-29 02:13:31 -0500

    yo

--- backend/src/routes/guides.js
+++ backend/src/routes/guides.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const GuideService = require("../services/guideService");
-const authMiddleware = require("../middleware/authMiddleware");
+const { authMiddleware } = require("../middleware/authMiddleware");
 
 // Get available guides
 router.get("/", async (req, res) => {
