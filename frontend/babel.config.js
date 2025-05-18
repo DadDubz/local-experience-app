27cf18f0fbb08842816a0702777964432f1f1441
DadDubz <daddubzonfb@gmail.com>
2025-05-18 00:39:55 -0500

    feat: Update Babel configuration and add React Native CLI as a dev dependency

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,5 +2,19 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@App': './App',
+            '@screens': './src/screens',
+            '@components': './src/components',
+            '@navigation': './src/navigation',
+            '@context': './src/context',
+          },
+        },
+      ],
+    ],
   };
 };
