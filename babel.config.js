375fb0a33f6322295ef3425d4108de6499dbc058
DadDubz <daddubzonfb@gmail.com>
2025-05-25 20:01:27 -0500

    fix: add alias for @App in Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -8,11 +8,12 @@ module.exports = function (api) {
         {
           extensions: ['.js', '.jsx', '.ts', '.tsx'],
           alias: {
+            '@App': './frontend/src/App',
             '@screens': './frontend/src/screens',
             '@components': './frontend/src/components',
             '@navigation': './frontend/src/navigation',
             '@context': './frontend/src/context',
-            '@services': './frontend/src/services', // ✅ Add this
+            '@services': './frontend/src/services',
           },
         },
       ],
